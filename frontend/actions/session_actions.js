1b6091cd398dcf4e8cd51aeca70e8ebe03423bf3
Joe D'Espinosa <jtdespinosa@gmail.com>
2019-01-22 17:15:48 -0500

    Write front-end auth, phase 1

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,47 @@
+import PostApiUtil from '../util/session_api_util';
+
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const login (user) => dispatch => {
+  return PostApiUtil.login(user).then(user => 
+    dispatch(
+      receiveCurrentUser(user)), 
+      err => (dispatch(receiveErrors(err.responseJSON)))
+    );
+};
+
+export const logout () => dispatch => {
+  return PostApiUtil.logout().then(user => 
+    dispatch(logoutCurrentUser()));
+};
+
+export const signup (user) => dispatch => {
+  return PostApiUtil.signup(user).then(user =>
+    dispatch(
+      receiveCurrentUser(user)),
+    err => (dispatch(receiveErrors(err.responseJSON)))
+  );
+};
+
+const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+const logoutCurrentUser = () => {
+  return {
+    type: LOGOUT_CURRENT_USER
+  };
+};
+
+const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
