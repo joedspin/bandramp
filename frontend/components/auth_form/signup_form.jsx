cf895d732853e41638ec3b07d489a60f0144fc95
Joe D'Espinosa <jtdespinosa@gmail.com>
2019-01-24 19:43:45 -0500

    Add footer and styling to signup page

--- frontend/components/auth_form/signup_form.jsx
+++ frontend/components/auth_form/signup_form.jsx
@@ -40,42 +40,47 @@ class SignupForm extends React.Component {
     return (
       <div className="auth-page">
         <AuthHeader />
-        <div className='login-form-container'>
-          <h2 className="form-head">Sign Up for a Label Account</h2>
-          <div className="top-rule"></div>
-          <form onSubmit={this.handleSubmit} className="login-form-box">
-            <div className="input-wrapper">
-              <label htmlFor="login-form-username">Username</label>
-              <input type="text" value={this.state.username} 
-                onChange={this.update('username')} 
-                id="login-form-username" />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="login-form-password">Password</label>
-                <input type="password" value={this.state.password} 
-                onChange={this.update('password')} 
-              id="login-form-password" />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="login-form-email">Email</label>
-                <input type="text" value={this.state.email}
-                  onChange={this.update('email')}
-              id="login-form-email" />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="login-form-email-confirm">Confirm email</label>
-                <input type="text" value={this.state.emailConfirm}
-                  onChange={this.update('emailConfirm')}
-                  id="login-form-email-confirm" />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="login-form-submit">&nbsp;</label>
-              <input type="submit" value={this.props.formType}
-                id="login-form-submit" />
-            </div>
-            {this.renderErrors()}
-            {this.props.navLink}
-          </form>
+        <div className="auth-page-content"></div>
+          <div className='login-form-container'>
+            <h2 className="form-head">Sign Up for a Label Account</h2>
+            <div className="top-rule"></div>
+            <form onSubmit={this.handleSubmit} className="login-form-box">
+              <div className="input-wrapper">
+                <label htmlFor="login-form-username">Username</label>
+                <input type="text" value={this.state.username} 
+                  onChange={this.update('username')} 
+                  id="login-form-username" />
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="login-form-password">Password</label>
+                  <input type="password" value={this.state.password} 
+                  onChange={this.update('password')} 
+                id="login-form-password" />
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="login-form-email">Email</label>
+                  <input type="text" value={this.state.email}
+                    onChange={this.update('email')}
+                id="login-form-email" />
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="login-form-email-confirm">Confirm email</label>
+                  <input type="text" value={this.state.emailConfirm}
+                    onChange={this.update('emailConfirm')}
+                    id="login-form-email-confirm" />
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="login-form-submit">&nbsp;</label>
+                <input type="submit" value={this.props.formType}
+                  id="login-form-submit" />
+              </div>
+              {this.renderErrors()}
+              {this.props.navLink}
+            </form>
+          </div>
+          <div className="auth-footer">
+
+          </div>
         </div>
       </div>
     );
